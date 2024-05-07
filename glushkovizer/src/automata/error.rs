3983de9fe586d069e1f3b9ddb4763f2a13c44f30
lurgrid <lurgrid@sagbot.com>
2024-05-07 17:21:15 +0200

    Small fix to idiomatic code in error handling

--- glushkovizer/src/automata/error.rs
+++ glushkovizer/src/automata/error.rs
@@ -1,31 +1,26 @@
 //! Module ayant pour but de facilité la gestion des erreurs qui peuvent arriver
 //! lors de la manipulation des automates
 
-use std::{error::Error, fmt::Display};
-
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 /// Enumeration des erreurs possible lors de la manipulation des automates
 pub enum AutomataError {
+    #[error("Unknow state 'from' given")]
     /// Erreur representant le fait que l'état qu'on veut modifer est inconnu
     UnknowStateFrom,
+    #[error("Unknow state 'to' given")]
     /// Erreur repsesentant le fait que l'état d'arrivé est inconnu
     UnknowStateTo,
+    #[error("Unknow state given")]
     /// Erreur representant le fait que l'état chercher n'existe pas
     UnknowState,
+    #[error("Not enough state given")]
     /// Erreur representant le fait qu'il n'y à pas assez d'état donné en
     /// parametre.
     NotEnoughState,
+    #[error("Duplicate state")]
     /// Erreur representant le fait qu'un état à un doublon
     DuplicateState,
 }
 
 /// Type alias des resultat des fonction de manipulation des automates
-pub type Result<T> = std::result::Result<T, AutomataError>;
-
-impl Display for AutomataError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
-
-impl Error for AutomataError {}
+pub type Result<T> = core::result::Result<T, AutomataError>;
